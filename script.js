dd4cc0449ed5cd093ffa17bfb34635191f45f1b1
CL02089 <csaba.lukacs@gmail.com>
2023-10-04 21:41:26 +0200

    Add value to buttons in HTML, basic JS

---
+++ script.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const display = document.querySelector("input");
+const numberButtons = document.querySelector(".number");
+const operationButtons = document.querySelector(".operation");
+const calculator = document.querySelector(".calc");
